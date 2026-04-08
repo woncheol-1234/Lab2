e1019bb04ba84b7c414959c6171601559daf6828
Hong <hwc0412@snu.ac.kr>
2026-04-08 12:46:37 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -7,7 +7,24 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
+	if (cnt == M){
+		for (int i : vec) cout << i << " " ;
+		cout << "\n";
+		return;
+	}
 
+	for (int i=1; i<= N; i++){
+		vec.push_back(i);
+		func(cnt+1);
+		vec.pop_back();
+	}
+}
+
+int main(){
+	cin >> N >> M;
+	func(0);
+	return 0;
+}
 
 
 
